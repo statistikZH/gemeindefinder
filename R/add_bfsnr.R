80ade2126d3d3875530ea57fae4a603021842e50
tknecht <thomas.knecht@statistik.ji.ktzh.ch>
2021-08-23 19:03:40 +0200

    debug system file paths

--- R/add_bfsnr.R
+++ R/add_bfsnr.R
@@ -1,15 +1,21 @@
 #' add bfsnr to geocoordinates
 #'
+#' @param df dataframe containing the coordinates which should be mapped to a bfsnr
+#'
+#' @param crs epsg number of the projection of the dataset
+#'
+#' @param datum datum as string, used to define which data should be used for the matching
+#'
 #' @export
 add_bfsnr <- function(df, crs, datum = "latest"){
   if(datum == "latest"){
-    existing_dirs <- list.dirs("inst/swissboundaries", recursive = F)
+    existing_dirs <- list.dirs(system.file("swissboundaries", package = "gemeindefinder"), recursive = F)
 
-    max_datum <-  max(as.Date(gsub("inst/swissboundaries/(.*)", "\\1", existing_dirs)))
+    max_datum <-  max(as.Date(gsub(".*/swissboundaries/(.*)", "\\1", existing_dirs)))
 
-    file_path <- paste0("inst/swissboundaries/", max_datum,"/SHAPEFILE_LV95_LN02/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp")
+    file_path <- paste0("swissboundaries/", max_datum,"/SHAPEFILE_LV95_LN02/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp")
   }else{
-    file_path <- paste0("inst/swissboundaries/", datum,"/SHAPEFILE_LV95_LN02/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp")
+    file_path <- paste0("swissboundaries/", datum,"/SHAPEFILE_LV95_LN02/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp")
   }
 
   if(!("GKODE" %in% names(df) & "GKODN" %in% names(df))){
@@ -24,7 +30,9 @@ add_bfsnr <- function(df, crs, datum = "latest"){
     ) %>%
     sf::st_transform(crs = 2056)
 
-  map <- sf::read_sf(file_path) %>%
+  map_path <- system.file(file_path, package = "gemeindefinder")
+
+  map <- sf::read_sf(map_path) %>%
     dplyr::select(NAME, BFS_NUMMER)
 
   df_bfsnr <- sf::st_intersection(df_sf, map) %>%
