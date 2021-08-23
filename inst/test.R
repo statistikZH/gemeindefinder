57a9affdcd7a697908fd041ec0897bf17c6dbbaf
tknecht <thomas.knecht@statistik.ji.ktzh.ch>
2021-08-23 18:33:55 +0200

    shapefiles

---
+++ inst/test.R
@@ -0,0 +1,14 @@
+dat <- jsonlite::fromJSON("https://data.geo.admin.ch/ch.bfe.ladestellen-elektromobilitaet/data/oicp/ch.bfe.ladestellen-elektromobilitaet.json")
+
+
+dat_info <- dat$EVSEData$EVSEDataRecord
+
+
+ladestationen_coords <- tibble::tibble(
+  geocoordinates = purrr::map(dat_info, "GeoCoordinates")
+) %>%
+  tidyr::unnest("geocoordinates") %>%
+  tidyr::separate(Google,c("GKODE","GKODN"),sep=" ",convert=TRUE) %>%
+  tibble::rownames_to_column()
+
+test <- add_bfsnr(ladestationen_coords, crs = 4326)
